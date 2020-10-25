6d889f3394fc276a0bf408bbb2de10679b72ee05
Carlos Diaz <diazpolanco13@gmail.com>
2020-10-25 09:47:37 -0400

    Primer commit -  inicio de proyecto

---
+++ src/CalendarApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CalendarApp = () => {
+    return (
+        <div>
+            <h1>Esto esta como lento</h1>
+        </div>
+    )
+}
---
+++ wallaby.js
@@ -0,0 +1,5 @@
+//Crea un archivo allaby.js en la carpeta principal con el siguiente contenido
+
+module.exports = () => ({
+    autoDetect: true,
+  });

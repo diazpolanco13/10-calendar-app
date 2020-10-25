494487d28769477972ed26e7d55fe58bd57b67d3
Carlos Diaz <diazpolanco13@gmail.com>
2020-10-25 19:38:20 -0400

    trasitionProfileOk

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -18,7 +18,7 @@ export const Navbar = () => {
               <div className="flex items-center mr-2 -ml-2 md:hidden">
                 {/* Mobile menu button */}
                 <button
-                  onClick={()=> setShowMenu(!showMenu) }
+                  onClick={() => setShowMenu(!showMenu)}
                   className="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
                   aria-label="Main menu"
                   aria-expanded="false"
@@ -149,72 +149,73 @@ export const Navbar = () => {
                 </button>
 
                 {/* Profile dropdown */}
-                <div className="relative ml-3">
-                  <div>
-                    <button
-                      className="flex text-sm transition duration-150 ease-in-out border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300"
-                      id="user-menu"
-                      aria-label="User menu"
-                      aria-haspopup="true"
-                    >
-                      <img
-                        className="w-8 h-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </button>
-                  </div>
-                  {/*
-              Profile dropdown panel, show/hide based on dropdown state.
-
-              Entering: "transition ease-out duration-200"
-                From: "transform opacity-0 scale-95"
-                To: "transform opacity-100 scale-100"
-              Leaving: "transition ease-in duration-75"
-                From: "transform opacity-100 scale-100"
-                To: "transform opacity-0 scale-95"
-            */}
-                  <div className="absolute right-0 w-48 mt-2 origin-top-right rounded-md shadow-lg">
-                    <div
-                      className="py-1 bg-white rounded-md shadow-xs"
-                      role="menu"
-                      aria-orientation="vertical"
-                      aria-labelledby="user-menu"
-                    >
-                      <a
-                        href="/"
-                        className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                        role="menuitem"
+                <Menu>
+                  {({ open }) => (
+                    <div className="relative ml-3">
+                      <div>
+                        <Menu.Button
+                          className="flex text-sm transition duration-150 ease-in-out border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300"
+                          id="user-menu"
+                          aria-label="User menu"
+                          aria-haspopup="true"
+                        >
+                          <img
+                            className="w-8 h-8 rounded-full"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        show={open}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
                       >
-                        Your Profile
-                      </a>
-                      <a
-                        href="/"
-                        className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                        role="menuitem"
-                      >
-                        Settings
-                      </a>
-                      <a
-                        href="/"
-                        className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                        role="menuitem"
-                      >
-                        Sign out
-                      </a>
+                        <div className="absolute right-0 w-48 mt-2 origin-top-right rounded-md shadow-lg">
+                          <div
+                            className="py-1 bg-white rounded-md shadow-xs"
+                            role="menu"
+                            aria-orientation="vertical"
+                            aria-labelledby="user-menu"
+                          >
+                            <Link
+                              to="/login"
+                              className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                              role="menuitem"
+                            >
+                              Your Profile
+                            </Link>
+                            <Link
+                              to="/"
+                              className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                              role="menuitem"
+                            >
+                              Settings
+                            </Link>
+                            <Link
+                              to="/"
+                              className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                              role="menuitem"
+                            >
+                              Sign out
+                            </Link>
+                          </div>
+                        </div>
+                      </Transition>
                     </div>
-                  </div>
-                </div>
+                  )}
+                </Menu>
               </div>
             </div>
           </div>
         </div>
 
-        {/*
-        Menú móvil, alternar clases según el estado del menú.
-        Menu open: "block", Menu closed: "hidden"
-        */}
-        <div className={`${showMenu ? "block md:hidden" : "hidden md:hidden" }`}>
+        {/* Menú móvil lateral, alternado por clases según el estado del menú.*/}
+        <div className={`${showMenu ? "block md:hidden" : "hidden md:hidden"}`}>
           <div className="pt-2 pb-3">
             <a
               href="/"
